25deb5c6248753e57fad21a8711eec797b245187
Aggeliki <angelaant45@gmail.com>
2020-07-20 03:34:21 +0300

    Day 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-window.onload=function()
-{if(window.jQuery){$(document).ready(function(){$(".sidebarNavigation .navbar-collapse").hide().clone().appendTo("body").removeAttr("class").addClass("sideMenu").show();$("body").append("<div class='overlay'></div>");$(".navbar-toggle, .navbar-toggler").on("click",function(){$(".sideMenu").addClass($(".sidebarNavigation").attr("data-sidebarClass"));$(".sideMenu, .overlay").toggleClass("open");$(".overlay").on("click",function(){$(this).removeClass("open");$(".sideMenu").removeClass("open")})});$("body").on("click",".sideMenu.open .nav-item",function(){if(!$(this).hasClass("dropdown")){$(".sideMenu, .overlay").toggleClass("open")}});$(window).resize(function(){if($(".navbar-toggler").is(":hidden")){$(".sideMenu, .overlay").hide()}else{$(".sideMenu, .overlay").show()}})})}else{console.log("sidebarNavigation Requires jQuery")}}
+window.onload = function () { if (window.jQuery) { $(document).ready(function () { $(".sidebarNavigation .navbar-collapse").hide().clone().appendTo("body").removeAttr("class").addClass("sideMenu").show(); $("body").append("<div class='overlay'></div>"); $(".navbar-toggle, .navbar-toggler").on("click", function () { $(".sideMenu").addClass($(".sidebarNavigation").attr("data-sidebarClass")); $(".sideMenu, .overlay").toggleClass("open"); $(".overlay").on("click", function () { $(this).removeClass("open"); $(".sideMenu").removeClass("open") }) }); $("body").on("click", ".sideMenu.open .nav-item", function () { if (!$(this).hasClass("dropdown")) { $(".sideMenu, .overlay").toggleClass("open") } }); $(window).resize(function () { if ($(".navbar-toggler").is(":hidden")) { $(".sideMenu, .overlay").hide() } else { $(".sideMenu, .overlay").show() } }) }) } else { console.log("sidebarNavigation Requires jQuery") } }
 
 
 window.addEventListener("load", function () {
@@ -7,8 +6,23 @@ window.addEventListener("load", function () {
 }
 )
 
-addEventListener("click", function (){
-    document.getElementById("newsletter")
-    alert("Enjoy our newsletter")
-})
 
+function myFunction() {
+    var txt;
+    if (confirm('Thank you for subscribing')) {
+        txt = "Thank you for subscribing";
+    }
+
+    document.getElementById("submit").innerHTML = txt;
+
+} 
+
+function myFunction() {
+    var txt;
+    if (confirm('Thank you for subscribing')) {
+        txt = "Thank you for subscribing";
+    }
+
+    document.getElementById("submitForm").innerHTML = txt;
+
+} 
